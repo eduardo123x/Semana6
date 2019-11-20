6fc01ea3447750bf03362d952ccf90664fbbae06
unknown <Admin@IHR80PBF441.continental.edu.pe>
2019-11-19 22:30:25 -0500

    Salto de Pantalla Usuario

--- app/src/main/java/com/example/semana6/AuthActivity.java
+++ app/src/main/java/com/example/semana6/AuthActivity.java
@@ -6,21 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AuthActivity extends AppCompatActivity {
     Button btnLogin;
+    EditText txtUser;
+    EditText txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         btnLogin=findViewById(R.id.btnLogin);
+        txtUser=findViewById(R.id.txtUser);
+        txtPassword=findViewById(R.id.txtPassword);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent s= new Intent(AuthActivity.this, MainActivity.class);
-                startActivity(s);
+                if (txtUser.getText().toString().equals("Bichito")) {
+                    Intent s = new Intent(AuthActivity.this, MainActivity.class);
+                    startActivity(s);
 
+                }
             }
         });
     }
