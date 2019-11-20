bdce4787a10a1b019c914efeb1b61ff851c019f9
unknown <Admin@IHR80PBF441.continental.edu.pe>
2019-11-19 22:24:09 -0500

    Auth Commit Salto de Pantalla

--- app/src/main/java/com/example/semana6/AuthActivity.java
+++ app/src/main/java/com/example/semana6/AuthActivity.java
@@ -2,13 +2,26 @@ package com.example.semana6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AuthActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent s= new Intent(AuthActivity.this, MainActivity.class);
+                startActivity(s);
+
+            }
+        });
     }
 }
