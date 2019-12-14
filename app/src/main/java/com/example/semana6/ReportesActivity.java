9fb40b58a588de9fe9edbbfc742a71e56e407bf4
unknown <Admin@IHR80PBF331.continental.edu.pe>
2019-12-14 11:57:33 -0500

    Presentar

--- app/src/main/java/com/example/semana6/ReportesActivity.java
+++ app/src/main/java/com/example/semana6/ReportesActivity.java
@@ -10,17 +10,30 @@ import android.widget.Button;
 public class ReportesActivity extends AppCompatActivity {
 
 Button btnVentas;
+Button btnClientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportes);
         btnVentas=findViewById(R.id.btnVentas);
 
+        btnClientes=findViewById(R.id.btnClientes);
+
         btnVentas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent s = new Intent(ReportesActivity.this, VentasActivity.class);
+                startActivity(s);
+
+            }
+        });
+
+        btnClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent s = new Intent(ReportesActivity.this, ClienteActivity.class);
                 startActivity(s);
 
             }
